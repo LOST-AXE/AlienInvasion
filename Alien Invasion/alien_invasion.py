ae9a0a8d83d0a2070aee8b909f8a2e9782403767
jiges <jigeshashemi1000@gmail.com>
2025-07-02 15:50:37 +0100

    Changed the image in ship.py and  updated the alien_invasion.py to display the image

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -14,6 +15,7 @@ class AlienInvasion:
                                                self.settings.screen_height))
 
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
     def run_game(self):
         """Start the main loop for the game"""
         while True:
@@ -23,6 +25,7 @@ class AlienInvasion:
                     sys.exit()
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 if __name__ == "__main__":
