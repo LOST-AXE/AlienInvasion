9420e35bc4a876e8fdee85df7a799619a333180b
jiges <jigeshashemi1000@gmail.com>
2025-07-03 08:44:01 +0100

    Added bullet class in bullet.py and added its settings in settings.py

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -7,5 +7,10 @@ class Settings:
         self.screen_width = 1275
         self.screen_height = 700
         self.bg_color = (4,10,46)
-        # ship setting
+        # Ship setting
         self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 102, 102)
