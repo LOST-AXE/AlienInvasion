5ff4a8780d651e0eb73e75d1810d449ea4e75c66
jiges <jigeshashemi1000@gmail.com>
2025-07-03 07:43:55 +0100

    Added ship range, changed ship speed through settings.py and changed size of display

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -4,6 +4,8 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 900
-        self.screen_height = 600
+        self.screen_width = 1275
+        self.screen_height = 700
         self.bg_color = (4,10,46)
+        # ship setting
+        self.ship_speed = 1.5
